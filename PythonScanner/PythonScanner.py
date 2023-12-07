f008e6f1d1df819b939a27814593ca1558d744d3
K <49505489+kelten5784@users.noreply.github.com>
2023-12-06 23:27:45 -0500

    SCAN MODES ADDED - PythonScanner.py
    added scan modes

--- PythonScanner/PythonScanner.py
+++ PythonScanner/PythonScanner.py
@@ -25,22 +25,22 @@ def scan_port(target, port):
 ## Given CODE
 def main():
     target = input("Enter the target IP address or hostname: ")
-    start_port = int(input("Enter the inital port: "))
-    end_port = int(input("Enter the last port: "))
-    
-    filter_option = input("Enter filter: (open/closed/all): ").lower()
+    scan_mode = input("Enter scan mode (quick/thorough): ").lower()
 
-    print(f"Scanning ports {start_port} to {end_port} on {target}... :3\n")
+    if scan_mode == 'quick':
+        filter_option = input("Enter filter: (open/closed/all): ").lower()
+        quick_scan(target, filter_option)
 
-    for port in range(start_port, end_port + 1):
-        if filter_option == 'open':
-            specific_ports(target, port, True)
-        elif filter_option == 'closed':
-            specific_ports(target, port, False)
-        elif filter_option == 'all':
-            scan_port(target, port)
-        else:
-            print("Invalid filter option. Please enter 'open', 'closed', or 'all'.")
+    elif scan_mode == 'thorough':
+        filter_option = input("Enter filter: (open/closed/all): ").lower()
+        start_port = int(input("Enter the initial port: "))
+        end_port = int(input("Enter the last port: "))
+        print(f"Scanning ports {start_port} to {end_port} on {target}...\n")
+        thorough_scan(target, start_port, end_port, filter_option)
+
+    else:
+        print("Invalid scan mode. Please enter 'quick' or 'thorough'.")
+
 ###################################################################################################################################
 ##1. Port Filtering
 def specific_ports(target, port, is_open):
@@ -71,7 +71,29 @@ def specific_ports(target, port, is_open):
         sock.close()
 ###################################################################################################################################
 ##2. Scan Modes
+def quick_scan(target, filter_option):
+    common_ports = [80, 443, 21, 22]
 
+    for port in common_ports:
+        if filter_option == 'open':
+            specific_ports(target, port, True)
+        elif filter_option == 'closed':
+            specific_ports(target, port, False)
+        elif filter_option == 'all':
+            scan_port(target, port)
+        else:
+            print("Invalid filter option. Please enter 'open', 'closed', or 'all'.")
+
+def thorough_scan(target, start_port, end_port, filter_option):
+    for port in range(start_port, end_port + 1):
+        if filter_option == 'open':
+            specific_ports(target, port, True)
+        elif filter_option == 'closed':
+            specific_ports(target, port, False)
+        elif filter_option == 'all':
+            scan_port(target, port)
+        else:
+            print("Invalid filter option. Please enter 'open', 'closed', or 'all'.")
 
 
 
